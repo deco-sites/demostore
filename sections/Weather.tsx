ae57c46c4b25d20030aeb051044053a533e4facd
gimenes <tlgimenes@gmail.com>
2024-05-21 09:04:43 -0300

    fix temperature

--- sections/Weather.tsx
+++ sections/Weather.tsx
@@ -1,12 +1,15 @@
-export { default as loader } from "apps/weather/loaders/temperature.ts"
+import { Temperature } from "apps/weather/loaders/temperature.ts";
 
 interface Props {
-
-  celsius?: string;
+  temperature?: Temperature | null;
 }
 
-export default function Section(props: Props) {
-  return <div class="w-full h-40 flex justify-center items-center">
-    <h2 class="text-5xl">Your Current Temperature is: {props.celsius}</h2>
-  </div>
+export default function Section({ temperature }: Props) {
+  return (
+    <div class="w-full h-40 flex justify-center items-center">
+      <h2 class="text-5xl">
+        Your Current Temperature is: {temperature?.celsius ?? "unknown"}
+      </h2>
+    </div>
+  );
 }

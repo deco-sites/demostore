207ea645f5068c1bb3d36cd237e544ddc03d1d42
igoramf <igoraaf1@gmail.com>
2026-04-27 19:01:15 -0300

    update store

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -28,7 +28,7 @@ import * as $ShowMore from "./islands/ShowMore.tsx";
 import * as $SliderJS from "./islands/SliderJS.tsx";
 import * as $WishlistButton_vtex from "./islands/WishlistButton/vtex.tsx";
 import * as $WishlistButton_wake from "./islands/WishlistButton/wake.tsx";
-import { type Manifest } from "$fresh/server.ts";
+import type { Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
